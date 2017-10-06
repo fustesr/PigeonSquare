64856c3e2061538258b481329e82d04c6a8d925e
Raph <raphael.fustes@gmail.com>
2017-10-06 11:42:15 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
+import java.awt.Dimension;
 
+import javax.swing.*;
 public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame jf = new JFrame("PigeonSquare");
+		jf.setSize(new Dimension(1200,800));
+		jf.setVisible(true);
+		
+		ImageIcon icone = new ImageIcon("res\\pigeon.png");
+		JLabel image = new JLabel(icone);
+		jf.add(image);
+		jf.setVisible(true);
+	
+	}
+	
 
 }
